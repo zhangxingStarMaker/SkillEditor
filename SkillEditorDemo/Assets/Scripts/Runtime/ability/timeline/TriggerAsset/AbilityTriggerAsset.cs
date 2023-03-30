0025b710ef16d5015631aa49af140b531770817f
“zhangxing <zhangxing03@bilibili.com>
2023-03-30 17:48:20 +0800

    修改路径

---
+++ SkillEditorDemo/Assets/Scripts/Runtime/ability/timeline/TriggerAsset/AbilityTriggerAsset.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+namespace AbilitySystem
+{
+    [System.Serializable]
+    public class AbilityTriggerAsset : PlayableAsset
+    {
+        public AbilityTriggerBehaviour TriggerBehaviour = new AbilityTriggerBehaviour();
+
+        public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+        {
+            return ScriptPlayable<AbilityTriggerBehaviour>.Create(graph, TriggerBehaviour);
+        }
+    }
+}
