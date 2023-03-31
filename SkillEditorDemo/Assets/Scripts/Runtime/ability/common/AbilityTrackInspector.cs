2ae737ef92472f329b6ff8f77e01021678e66288
“zhangxing <zhangxing03@bilibili.com>
2023-03-31 17:05:53 +0800

    增加选择赛道的时候选择对应的Clip

--- SkillEditorDemo/Assets/Scripts/Runtime/ability/common/AbilityTrackInspector.cs
+++ SkillEditorDemo/Assets/Scripts/Runtime/ability/common/AbilityTrackInspector.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine.Serialization;
 using UnityEngine.Timeline;
@@ -10,8 +11,10 @@ namespace AbilitySystem
     {
         [FormerlySerializedAs("TrackAsset")] public TrackAsset CurrentTrackAsset;
         [FormerlySerializedAs("CurrentTrackAssets")] public List<TrackAsset> CurrentTimelineTrackAssets = new List<TrackAsset>();
-        private int _selectIndex = 0;
+        private int _selectTrackIndex = 0;
+        private int _selectClipIndex = 0;
         public TrackAsset SelectTrackAsset = null;
+        public TimelineClip SelectTimelineClip = null;
         
 
         private void OnEnable()
@@ -27,6 +30,7 @@ namespace AbilitySystem
             CurrentTrackAsset = (TrackAsset)this.target;
             CurrentTimelineTrackAssets.Clear();
             var trackAssets = CurrentTrackAsset.timelineAsset.GetOutputTracks();
+            CurrentTimelineTrackAssets.Add(null);
             foreach (var trackAsset in trackAssets)
             {
                 if (trackAsset is not MarkerTrack && trackAsset!=CurrentTrackAsset)
@@ -34,25 +38,57 @@ namespace AbilitySystem
                     CurrentTimelineTrackAssets.Add(trackAsset);
                 }
             }
-
-            if (CurrentTimelineTrackAssets.Count>0)
-            {
-                SelectTrackAsset = CurrentTimelineTrackAssets[_selectIndex];
-            }
         }
 
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
+            ShowSelectTrack();
+            ShowSelectClip();
+        }
+
+        /// <summary>
+        /// 显示选择的轨道数据
+        /// </summary>
+        private void ShowSelectTrack()
+        {
             if (CurrentTimelineTrackAssets!=null&& CurrentTimelineTrackAssets.Count>0)
             {
                 List<string> trackAssetNames = new List<string>();
                 foreach (var trackAsset in CurrentTimelineTrackAssets)
                 {
-                    trackAssetNames.Add(trackAsset.name);
+                    if (trackAsset==null)
+                    {
+                        trackAssetNames.Add("无选择");
+                    }
+                    else
+                    {
+                        trackAssetNames.Add(trackAsset.name);
+                    }
                 }
-                _selectIndex = EditorGUILayout.Popup("选择依赖赛道",_selectIndex, trackAssetNames.ToArray());
-                SelectTrackAsset = CurrentTimelineTrackAssets[_selectIndex];
+                _selectTrackIndex = EditorGUILayout.Popup("选择依赖赛道",_selectTrackIndex, trackAssetNames.ToArray());
+                SelectTrackAsset = CurrentTimelineTrackAssets[_selectTrackIndex];
+            }
+        }
+
+        /// <summary>
+        /// 挑选选中的Track中的Clip的数据
+        /// </summary>
+        private void ShowSelectClip()
+        {
+            if (SelectTrackAsset!=null)
+            {
+                var trackClips = SelectTrackAsset.GetClips().ToArray();
+                if (trackClips.Length>0)
+                {
+                    List<string> trackClipNames = new List<string>(); 
+                    foreach (var trackClip in trackClips)
+                    {
+                        trackClipNames.Add(trackClip.displayName);
+                        _selectClipIndex = EditorGUILayout.Popup("选择依赖赛道", _selectClipIndex, trackClipNames.ToArray());
+                    }
+                    SelectTimelineClip = trackClips[_selectClipIndex];
+                }
             }
         }
     }
